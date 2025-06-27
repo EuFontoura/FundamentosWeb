260289f3e68e41f2a51e4a36bb447ee3bc59d6dd
Gabriel Fontoura <gabrielfmendonca@gmail.com>
2025-06-27 09:23:29 -0300

    docs: adicionando comentarios no codigo para requisitos do trabalho da faculdade

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+// Para controlar a abertura e fechamento da navbar em mobile
+
 var NavbarButton = document.getElementById("mobile-menu");
 var CloseButton = document.getElementById("close");
+
+/**
+ * Exibe o menu lateral (sidebar)
+ * Remove a classe 'hide' e adiciona a classe 'active'
+ */
 
 NavbarButton.addEventListener("click", function () {
   let sidebar = document.getElementById("sidebar");
   sidebar.classList.remove("hide");
   sidebar.classList.add("active");
 });
+
+/**
+ * Oculta o menu lateral (sidebar)
+ * Remove a classe 'active' e adiciona a classe 'hide'
+ */
 
 CloseButton.addEventListener("click", function () {
   let sidebar = document.getElementById("sidebar");
